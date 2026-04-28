9ccdff9771ecd3608b9b32ea33b07c5e107498d9
Madi-SoilihiBounouBen <madisoilihi1999@gmail.com>
2026-04-28 16:34:40 +0300

    Premier pas sur REACT. Création du fichier Banner

---
+++ la-maison-jungle/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Banner from './components/Banner'
+
+function App() {
+ import Banner from './components/Banner'
+
+  return (
+    <>
+      <Banner />
+    
+    </>
+  )
+}
+
+export default App
---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+/**
+ * Banner est le composant pour présenter la bannière (la barre de navigation)
+ * Le composant Banner retourne un message codé en HTML <h1>Bienvenue à la maison !</h1>
+ * Le composant Banner est exporté pour être utilisé dans d'autres fichiers
+ */
+// Je déclare mon Banner, qui est une fonction qui retourne du JSX (HTML dans du JavaScript)
+const Banner = () => {
+    return <h1>Bienvenue à la maison !</h1>
+}
+
+/*
+* J'exporte le composant Banner, en utilisant les mots-clés export default suivis du nom du composant 
+*/
+export default Banner
